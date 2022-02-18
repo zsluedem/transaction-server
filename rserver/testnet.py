7b892a8cde3b90388c7be90cbd7bbb28b2903f03
Will Qiu <zsluedem06@gmail.com>
2022-02-18 14:17:06 +0800

    update faucet vue2 cdn

--- rserver/testnet.py
+++ rserver/testnet.py
@@ -30,7 +30,7 @@ def faucetPage():
 <html>
   <head>
     <title>TestNet Faucet for RChain</title>
-    <script src="https://unpkg.com/vue"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   </head>
   <body>
